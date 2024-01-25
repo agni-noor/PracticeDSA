e4998cf6f0d08606a0c9ad6caa473b8c1adbc875
Agni Noor <agninoor04@gmail.com2>
2024-01-25 23:01:07 +0600

    Stack Completed

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -96,11 +96,96 @@ public:
         {
             cout << arr[i] << "  ";
         }
+        cout << endl;
     }
 };
 
 int main()
 {
+    Stack s1;
+    int option, position, value;
+    do
+    {
+        cout << "What operation do you want to perform? Select option number. Enter 0 to Exit." << endl;
+        cout << "1. push()" << endl;
+        cout << "2. pop()" << endl;
+        cout << "3. isEmpty()" << endl;
+        cout << "4. isFull()" << endl;
+        cout << "5. peek()" << endl;
+        cout << "6. count()" << endl;
+        cout << "7. change()" << endl;
+        cout << "8. display()" << endl;
+        cout << "9. Clear Screen" << endl;
+
+        cin >> option;
+        switch (option)
+        {
+        case 0:
+            break;
+        case 1:
+            cout << "Enter an item to push to the stack:" << endl;
+            cin >> value;
+            s1.push(value);
+            break;
+
+        case 2:
+            cout << "Pop function called - popped value :" << s1.pop() << endl;
+            break;
+
+        case 3:
+            if (s1.isEmpty())
+            {
+                cout << "The stack is empty" << endl;
+            }
+            else
+                cout << "The stack is not empty" << endl;
+
+            break;
+
+        case 4:
+            if (s1.isFull())
+            {
+                cout << "Stack is full" << endl;
+            }
+            else
+                cout << "Stack is not full" << endl;
+            break;
+
+        case 5:
+            cout << "Enter position to peek at:" << endl;
+            cin >> position;
+            cout << "The value at the given position is : " << s1.peek(position) << endl;
+            break;
+
+        case 6:
+            cout << "Total number of elements present in the stack:" << s1.count() << endl;
+            break;
+
+        case 7:
+            cout << "Enter the position that is to be changed" << endl;
+            cin >> position;
+
+            cout << "Enter the changed value:" << endl;
+            cin >> value;
+            s1.change(position, value);
+
+            break;
+
+        case 8:
+            cout << "Display function called" << endl;
+            s1.display();
+            break;
+
+        case 9:
+            system("cls");
+            break;
+
+        default:
+            cout << "Enter proper option number" << endl;
+            break;
+        }
+
+    } while (option != 0);
 
     return 0;
 }
