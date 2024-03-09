82cf786a4bbad4fe73b4523efb894510c40c3df3
Agni Noor <agninoor04@gmail.com>
2024-03-09 10:44:46 +0600

    Selection sort in ascending order

---
+++ Array/Sorting/SelectionSort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionSort(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        int min_index = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[min_index])
+            {
+                min_index = j;
+            }
+        }
+        swap(&arr[min_index], &arr[i]);
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {8, 9, 4, 6, 7, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printArray(arr, size);
+    selectionSort(arr, size);
+    cout << "Array sorted in ascending order using selection sort:" << endl;
+    printArray(arr, size);
+    return 0;
+}
