dc2bff3a5c4b542e68781eccd8bab21afad39717
Agni Noor <agninoor04@gmail.com>
2024-04-09 02:31:11 +0600

    sort STL

---
+++ STL/introsort.cpp
@@ -0,0 +1,4 @@
+// Introsort --> the sorting used in sort() in STL cpp
+// Uses three sorting algorithms --> Quicksort, Heapsort and Insertion sort
+// starts with quicksort, if recursion depth is high, swithes to heapsort, if small number of elements, then insertion sort
+// Worst case time complexity --> nlogn
